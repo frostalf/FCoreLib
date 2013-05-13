9087775b9fb0ca502942ce6beef289f28094cdfd
Luis <lgpserranegra@hotmail.com>
2013-05-13 07:32:43 -0300

    Fixed string object handling

--- src/me/FurH/Core/object/ObjectUtils.java
+++ src/me/FurH/Core/object/ObjectUtils.java
@@ -1,14 +1,15 @@
 package me.FurH.Core.object;
 
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.math.BigInteger;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 import me.FurH.Core.exceptions.CoreException;
 import me.FurH.Core.file.FileUtils;
-import me.FurH.Core.inventory.InventoryStack;
 
 /**
  *
@@ -25,28 +26,34 @@ public class ObjectUtils {
      * @throws CoreException  
      */
     public static String getStringFromObject(Object object) throws CoreException {
-        String ret = null;        
-
+        String ret = null;
+        
         ByteArrayOutputStream baos = null;
+        GZIPOutputStream gos = null;
         ObjectOutputStream oos = null;
-
+        
         try {
 
             baos = new ByteArrayOutputStream();
-            oos = new ObjectOutputStream(baos);
-            
-            oos.writeObject(object);
-            oos.flush(); baos.flush();
+            gos = new GZIPOutputStream(baos);
+            oos = new ObjectOutputStream(gos);
 
-            ret = new BigInteger(1, baos.toByteArray()).toString(32);
+            oos.writeObject(object);
+
+            oos.flush();
+            gos.flush();
+            baos.flush();
+
+            ret = encode(baos.toByteArray());
         } catch (IOException ex) {
             throw new CoreException(ex, "Failed to parse object '" + object.getClass().getSimpleName() + "' to a string");
         } finally {
             FileUtils.closeQuietly(baos);
+            FileUtils.closeQuietly(gos);
             FileUtils.closeQuietly(oos);
         }
-
-        return encode(ret);
+        
+        return ret;
     }
 
     /**
@@ -60,30 +67,33 @@ public class ObjectUtils {
         Object ret = null;
 
         ByteArrayInputStream bais = null;
-        ObjectInputStream dis = null;
+        GZIPInputStream gis = null;
+        ObjectInputStream ois = null;
 
         try {
 
-            bais = new ByteArrayInputStream(new BigInteger(decode(string), 32).toByteArray());
-            dis = new ObjectInputStream(bais);
+            bais = new ByteArrayInputStream(decode(string));
+            gis = new GZIPInputStream(bais);
+            ois = new ObjectInputStream(gis);
 
-            ret = dis.readObject();
+            ret = ois.readObject();
 
         } catch (Exception ex) {
             throw new CoreException(ex, "Failed to parse string '" + string + "' into an object");
         } finally {
             FileUtils.closeQuietly(bais);
-            FileUtils.closeQuietly(dis);
+            FileUtils.closeQuietly(gis);
+            FileUtils.closeQuietly(ois);
         }
 
         return ret;
     }
-    
-    private static String encode(String string) {
-        return InventoryStack.encode(string);
+
+    public static String encode(byte[] data) {
+        return Base64.encode(data);
     }
 
-    private static String decode(String string) {
-        return InventoryStack.decode(string);
+    public static byte[] decode(String string) {
+        return Base64.decode(string);
     }
 }
