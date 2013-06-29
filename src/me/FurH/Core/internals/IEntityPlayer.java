037254a45959843a92b8cf76f758a064c66d9440
Luis <lgpserranegra@hotmail.com>
2013-06-29 18:23:46 -0300

    Rollback: Add method with the settings packet

--- src/me/FurH/Core/internals/IEntityPlayer.java
+++ src/me/FurH/Core/internals/IEntityPlayer.java
@@ -9,7 +9,6 @@ import net.minecraft.server.v1_5_R3.EntityPlayer;
 import net.minecraft.server.v1_5_R3.ItemStack;
 import net.minecraft.server.v1_5_R3.Packet;
 import net.minecraft.server.v1_5_R3.Packet0KeepAlive;
-import net.minecraft.server.v1_5_R3.Packet204LocaleAndViewDistance;
 import net.minecraft.server.v1_5_R3.Packet250CustomPayload;
 import net.minecraft.server.v1_5_R3.Packet51MapChunk;
 import net.minecraft.server.v1_5_R3.Packet56MapChunkBulk;
@@ -32,7 +31,7 @@ public abstract class IEntityPlayer {
     protected boolean inventory_hidden = false;
     protected EntityPlayer entity;
     protected Player player;
-    
+
     /**
      * Set the Player of this IEntityPlayer object
      * 
@@ -135,8 +134,7 @@ public abstract class IEntityPlayer {
             PacketManager.callAsyncCustomPayload(player, p250.data, p250.length, p250.tag);
         } else
         if (packet.n() == 204) {
-            Packet204LocaleAndViewDistance p204 = (Packet204LocaleAndViewDistance) packet;
-            PacketManager.callAsyncClientSettings(player, p204);
+            PacketManager.callAsyncClientSettings(player);
         }
     }
 
