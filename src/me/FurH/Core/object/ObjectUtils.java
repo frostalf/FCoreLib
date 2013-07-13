9b7f776fd09de99804595d64afa36488079ea4ef
Luis <lgpserranegra@hotmail.com>
2013-07-13 06:09:50 -0300

    Added write/read objects from files

--- src/me/FurH/Core/object/ObjectUtils.java
+++ src/me/FurH/Core/object/ObjectUtils.java
@@ -2,9 +2,13 @@ package me.FurH.Core.object;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 import me.FurH.Core.exceptions.CoreException;
@@ -17,6 +21,77 @@ import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
  * All Rights Reserved unless otherwise explicitly stated.
  */
 public class ObjectUtils {
+
+    /**
+     * Get the object from the given file
+     *
+     * @param file the file to read
+     * @return the object
+     * @throws CoreException if the file is empty or has more than one object
+     */
+    public static Object getObjectFromFile(File file) throws CoreException {
+        
+        List<String> line = FileUtils.getLinesFromFile(file);
+        if (line.isEmpty()) {
+            throw new CoreException("There is nothing written in this file: " + file.getAbsolutePath());
+        }
+        
+        if (line.size() > 1) {
+            throw new CoreException("This file has more than one line written: " + file.getAbsolutePath());
+        }
+        
+        return getObjectFromString(line.get(0));
+    }
+
+    /**
+     * Save the object to a file
+     *
+     * @param file the file to write
+     * @param object the object to write in the file
+     * @throws CoreException
+     */
+    public static void saveObjectToFile(File file, Object object) throws CoreException {
+        FileUtils.setLinesOfFile(file, new ArrayList<String>(Arrays.asList(new String[] { getStringFromObject(object) })));
+    }
+    
+    /**
+     * Get all objects from the file
+     *
+     * @param file the file to read the objects
+     * @return an object array containing all the objects, it will always be on the sequence they was written.
+     * @throws CoreException if the file was empty
+     */
+    public static Object[] getObjectsFromFile(File file) throws CoreException {
+        
+        List<String> lines = FileUtils.getLinesFromFile(file);
+        if (lines.isEmpty()) {
+            throw new CoreException("There is nothing written in this file: " + file.getAbsolutePath());
+        }
+
+        List<Object> objects = new ArrayList<Object>();
+        for (String line : lines) {
+            objects.add(getObjectFromString(line));
+        }
+        
+        return objects.toArray();
+    }
+    
+    /**
+     * Save all objects to a file
+     *
+     * @param file the file to be written
+     * @param objects the objects to write in the file, the sequence will be preserved
+     * @throws CoreException
+     */
+    public static void saveObjectsToFile(File file, Object[] objects) throws CoreException {
+        List<String> lines = new ArrayList<String>();
+
+        for (Object o : objects) {
+            lines.add(getStringFromObject(o));
+        }
+
+        FileUtils.setLinesOfFile(file, lines);
+    }
 
     /**
      * Get the string representation of an object
