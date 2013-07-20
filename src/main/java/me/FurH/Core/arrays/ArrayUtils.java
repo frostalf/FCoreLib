2884c719f5bef62056eabe67c4d98bbeee31e419
Luis <lgpserranegra@hotmail.com>
2013-07-20 15:47:59 -0300

    Added join and split array methods

--- src/main/java/me/FurH/Core/arrays/ArrayUtils.java
+++ src/main/java/me/FurH/Core/arrays/ArrayUtils.java
@@ -1,6 +1,7 @@
 package me.FurH.Core.arrays;
 
 import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
 import java.util.zip.Deflater;
 import java.util.zip.Inflater;
 import me.FurH.Core.exceptions.CoreException;
@@ -13,6 +14,62 @@ import me.FurH.Core.file.FileUtils;
  */
 public class ArrayUtils {
 
+    /**
+     * Join the 2D array into a single array
+     *
+     * @param buffer the 2D array to join
+     * @return a single array with all the 2D array data
+     */
+    public static byte[] joinByteArray(byte[][] buffer) {
+
+        int lenght = 0;
+        for (byte[] slice : buffer) {
+            lenght += slice.length;
+        }
+
+        byte[] ret = new byte [ lenght ];
+        int read = 0;
+
+        for (int j1 = 0; j1 < buffer.length; j1++) {
+            byte[] copy = buffer[j1];
+
+            System.arraycopy(copy, 0, ret, read, copy.length);
+
+            read += copy.length;
+        }
+
+        return ret;
+    }
+
+    /**
+     * Split the byte array into pieces of the given size, the last piece may not have the same size of the others.
+     *
+     * @param array the array to split
+     * @param each the size of each piece
+     * @return the 2D array with the pieces
+     */
+    public static byte[][] splitByteArray(byte[] array, int each) {
+        
+        int slice = (int) Math.floor(array.length / each);
+        byte[][] slices = new byte[ slice + 1 ][];
+
+        int read = 0;
+
+        for (int j1 = 0; j1 < slices.length; j1++) {
+
+            int to = read + each;
+
+            if (to > array.length) {
+                to = array.length;
+            }
+
+            slices[j1] = Arrays.copyOfRange(array, read, to);
+            read += each;
+        }
+
+        return slices;
+    }
+    
     /**
      * Compress the given byte array using the java.util.zip.Deflater
      *
