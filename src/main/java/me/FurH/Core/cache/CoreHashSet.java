1e765b4f759d62047f0a9f40186252a4664b028e
Luis <lgpserranegra@hotmail.com>
2013-07-20 22:58:20 -0300

    Actually add the method to set the CoreHashSet as a softcache to be cleaned up.

--- src/main/java/me/FurH/Core/cache/CoreHashSet.java
+++ src/main/java/me/FurH/Core/cache/CoreHashSet.java
@@ -3,6 +3,7 @@ package me.FurH.Core.cache;
 import java.util.Collection;
 import java.util.HashSet;
 import me.FurH.Core.gc.IMemoryMonitor;
+import me.FurH.Core.gc.MemoryMonitor;
 
 /**
  *
@@ -13,6 +14,7 @@ import me.FurH.Core.gc.IMemoryMonitor;
 public class CoreHashSet<E> extends HashSet<E> implements IMemoryMonitor {
 
     private static final long serialVersionUID = -3587365391140026898L;
+    private boolean softCache = false;
 
     public CoreHashSet(Collection<? extends E> c) {
         super(c);
@@ -20,6 +22,19 @@ public class CoreHashSet<E> extends HashSet<E> implements IMemoryMonitor {
 
     public CoreHashSet() {
         super();
+    }
+
+    /**
+     * Set this cache to soft cache, it means that it can be automatically cleaned out to release memory.
+     *
+     * @param softCache true if this is a soft cache, false otherwise.
+     */
+    public void setSoftCache(boolean softCache) {
+        this.softCache = softCache;
+
+        if (this.softCache) {
+            MemoryMonitor.register(this);
+        }
     }
 
     @Override
