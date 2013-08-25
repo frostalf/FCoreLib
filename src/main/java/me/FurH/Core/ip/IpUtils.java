7cc3e79e05b1f6a892338baabe4424ae646acb19
Luis <lgpserranegra@hotmail.com>
2013-08-25 17:49:30 -0300

    JavaDocs

--- src/main/java/me/FurH/Core/ip/IpUtils.java
+++ src/main/java/me/FurH/Core/ip/IpUtils.java
@@ -14,10 +14,10 @@ public class IpUtils {
     private static SoftReference<Pattern> IPv6;
 
     /**
-     * Check if the string is a valid ip address, ipv4 or ipv6
+     * Check if the string is a valid IP address, IPv4 or IPv6
      *
      * @param ip the string to check
-     * @return true if it is a ipv4 or ipv6, false otherwise.
+     * @return true if it is a IPv4 or IPv6, false otherwise.
      */
     public static boolean isAnyIp(String ip) {
         return isIPv4(ip) || isIPv6(ip);
@@ -26,7 +26,7 @@ public class IpUtils {
     /**
      * Get if the given string is an IPv4 address
      * 
-     * @param ip the ip string
+     * @param ip the IP string
      * @return true if the string is an IPv4 address, false otherwise.
      */
     public static boolean isIPv4(String ip) {
@@ -42,7 +42,7 @@ public class IpUtils {
     /**
      * Get if the given string is an IPv6 address
      * 
-     * @param ip the ip string
+     * @param ip the IP string
      * @return true if the string is an IPv6 address, false otherwise.
      */
     public static boolean isIPv6(String ip) {
@@ -55,30 +55,30 @@ public class IpUtils {
     }
     
     /**
-     * Replace the . of the ip to - to allow better database compatibility
+     * Replace the . of the IP to - to allow better database compatibility
      * 
-     * @param ip the ip to replace
-     * @return the ip with the dots replaced
+     * @param ip the IP to replace
+     * @return the IP with the dots replaced
      */
     public static String getIpFromDb(String ip) {
         return ip.replaceAll("-", ".");
     }
     
     /**
-     * Replace the - of the ip to . to retrieve the original ip after the usage of the @getIpFromDb method
+     * Replace the - of the IP to . to retrieve the original IP after the usage of the @getIpFromDb method
      * 
-     * @param ip the ip to restore
-     * @return the ip with the dots restored
+     * @param ip the IP to restore
+     * @return the IP with the dots restored
      */
     public static String getIpToDb(String ip) {
         return ip.replaceAll("\\.", "-");
     }
 
     /**
-     * Get the decimal representation of the given ip
+     * Get the decimal representation of the given IP
      * 
-     * @param ip the ip address
-     * @return the decimal representation of the ip
+     * @param ip the IP address
+     * @return the decimal representation of the IP
      */
     public static long ipToDecimal(String ip) {
         long result = 0;
@@ -92,10 +92,10 @@ public class IpUtils {
     }
 
     /**
-     * Get the ip address from the decimal representation
+     * Get the IP address from the decimal representation
      * 
-     * @param ip the decimal ip
-     * @return the ip address
+     * @param ip the decimal IP
+     * @return the IP address
      */
     public static String decimalToIp(long ip) {
         StringBuilder sb = new StringBuilder(15);
@@ -114,10 +114,10 @@ public class IpUtils {
     }
     
     /**
-     * Get the player ip address
+     * Get the player IP address
      * 
-     * @param player the player to get the ip
-     * @return the ip address without ports
+     * @param player the player to get the IP
+     * @return the IP address without ports
      */
     public static String getPlayerIp(Player player) {
         return player.getAddress().toString().substring(1, player.getAddress().toString().indexOf(':'));
